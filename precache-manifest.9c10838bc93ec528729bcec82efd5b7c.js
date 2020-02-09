215564924def7d736161fa55757deffd3a64c783
acting93 <53194530+acting93@users.noreply.github.com>
2020-02-10 00:50:47 +0100

    Updates

---
+++ precache-manifest.9c10838bc93ec528729bcec82efd5b7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af0188b5310e9203f8796979097fae8",
+    "url": "/MoJAArchitekci/index.html"
+  },
+  {
+    "revision": "c017c7a3548295c7ddde",
+    "url": "/MoJAArchitekci/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fcce1501a822d68d0560",
+    "url": "/MoJAArchitekci/static/css/main.d3cd94cb.chunk.css"
+  },
+  {
+    "revision": "c017c7a3548295c7ddde",
+    "url": "/MoJAArchitekci/static/js/2.26a09a0d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/MoJAArchitekci/static/js/2.26a09a0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcce1501a822d68d0560",
+    "url": "/MoJAArchitekci/static/js/main.5cc755f7.chunk.js"
+  },
+  {
+    "revision": "d4bae06f70eb06d6460d",
+    "url": "/MoJAArchitekci/static/js/runtime-main.d32838a1.js"
+  },
+  {
+    "revision": "fd2aaf917935b799711f6c89dba53ae1",
+    "url": "/MoJAArchitekci/static/media/background3.fd2aaf91.jpg"
+  },
+  {
+    "revision": "81641d1db87025e39ea6eae947aeb890",
+    "url": "/MoJAArchitekci/static/media/inverso.81641d1d.png"
+  },
+  {
+    "revision": "3aaaf6ee21eea960ef4916b7463da868",
+    "url": "/MoJAArchitekci/static/media/jumbotron.3aaaf6ee.jpg"
+  },
+  {
+    "revision": "960b5b4cfb3238345ba1fb21010ea807",
+    "url": "/MoJAArchitekci/static/media/logo.960b5b4c.png"
+  }
+]);
